589864443a568cbc977d340568b57b98e970bcc4
FengHShia <50110285+fenghshia@users.noreply.github.com>
2021-01-05 21:08:04 +0800

    修改
    修改

--- set_up_frp.sh
+++ set_up_frp.sh
@@ -1,8 +1,8 @@
-wget https://github.com/fatedier/frp/releases/download/v0.34.3/frp_0.20.0_linux_amd64.tar.gz
+wget https://github.com/fatedier/frp/releases/download/v0.20.0/frp_0.20.0_linux_amd64.tar.gz
 echo "unzip frp"
 tar -xvmzf frp_0.20.0_linux_amd64.tar.gz
 rm -rf frp_0.20.0_linux_amd64.tar.gz
-mv frp_0.34.3_linux_amd64 frp
+mv frp_0.20.0_linux_amd64 frp
 echo "modify config"
 sed -i "s/dashboard_pwd = admin/dashboard_pwd = 89948632/g" ./frp/frps_full.ini
 sed -i "s/token = 12345678/token = xuan89948632./g" ./frp/frps_full.ini
@@ -31,19 +31,3 @@ echo "esac" >> ./frp/frpctl
 echo "start frp"
 cd frp
 ./frpctl start
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
