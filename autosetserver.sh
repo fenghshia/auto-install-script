bb6dab7c0b11ef4f776429175fdd64f1a2e5db3d
泷川太一 <fenghshia@163.com>
2021-02-06 21:51:12 +0800

    nextcloud自动部署

--- autosetserver.sh
+++ autosetserver.sh
@@ -1,10 +1,54 @@
+sudo mkdir /opt/dataroot
+
+# install nextcloud
+sudo apt install -y apache2 mariadb-server libapache2-mod-php7.4
+sudo apt install -y php7.4-gd php7.4-mysql php7.4-curl php7.4-mbstring php7.4-intl
+sudo apt install -y php7.4-gmp php7.4-bcmath php-imagick php7.4-xml php7.4-zip
+
+sudo /etc/init.d/mysql start
+sudo mysql -uroot -p -e "CREATE USER 'fenghshia'@'localhost' IDENTIFIED BY '89948632';"
+sudo mysql -uroot -p -e "CREATE DATABASE IF NOT EXISTS nextcloud CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
+sudo mysql -uroot -p -e "GRANT ALL PRIVILEGES ON nextcloud.* TO 'fenghshia'@'localhost';"
+sudo mysql -uroot -p -e "FLUSH PRIVILEGES;"
+
+sudo wget https://download.nextcloud.com/server/releases/nextcloud-20.0.7.zip
+sudo unzip nextcloud-20.0.7.zip
+sudo cp -r nextcloud /var/www
+sudo rm -rf nextcloud-20.0.7.zip
+
+sudo touch /etc/apache2/sites-available/nextcloud.conf
+sudo echo "Alias /nextcloud \"/var/www/nextcloud/\"
+<Directory /var/www/nextcloud/>
+    Require all granted
+    AllowOverride All
+    Options FollowSymLinks MultiViews
+			
+    <IfModule mod_dav.c>
+        Dav off
+    </IfModule>
+</Directory>
+" > /etc/apache2/sites-available/nextcloud.conf
+
+sudo a2ensite nextcloud.conf
+sudo a2enmod rewrite
+sudo a2enmod headers
+sudo a2enmod env
+sudo a2enmod dir
+sudo a2enmod mime
+sudo a2enmod setenvif
+
+cd /var/www/nextcloud/
+sudo chmod -R 777 /var/www/nextcloud
+sudo -u www-data php occ  maintenance:install --database "mysql" --database-name "nextcloud"  --database-user "fenghshia" --database-pass "89948632" --admin-user "fenghshia" --admin-pass "xuan89948632." --data-dir "/opt/dataroot"
+cd ~
+
+service apache2 restart
+
 # install bbr
-sudo mkdir dataroot
 sudo wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
 sudo chmod 777 bbr.sh
 sudo ./bbr.sh
 sudo rm -rf bbr.sh
-sudo mv install_bbr.log dataroot
 # echo "是否重启系统: 1. 重启 2.不重启"
 # read -p "Enter your name age id_card address:" n
 # case $n in
@@ -16,7 +60,7 @@ sudo wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercon
 sudo chmod 777 shadowsocks-all.sh
 sudo ./shadowsocks-all.sh
 sudo rm -rf shadowsocks-all.sh
-sudo mv shadowsocks_r_qr.png dataroot
+sudo chmod 777 shadowsocks_r_qr.png
+sudo mv shadowsocks_r_qr.png /opt/dataroot/fenghshia/files
 
 sudo chmod -R 777 dataroot
-
