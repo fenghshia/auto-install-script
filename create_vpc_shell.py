069835685533cbcfb948bf1fa071b3464d2aca38
泷川太一 <754587525@qq.com>
2021-04-20 15:34:52 +0800

    vpc自动脚本优化

--- create_vpc_shell.py
+++ create_vpc_shell.py
@@ -1,101 +1,118 @@
+import re
 from copy import deepcopy
+from datetime import datetime
 
+class vpccomplete:
 
-routebase = [
-    "gcloud",
-    "compute",
-    "--project={}",
-    "firewall-rules",
-    "create"
-]
+    def __init__(self, custom, code):
+        # 解析code获取 项目名 账户名
+        project_re = '--project=(.*) instances'
+        server_account_re = '--service-account=(.*) --scopes='
+        image_re = '--image=(.*) --image-project='
+        res = re.search(project_re, code, re.M|re.I)
+        self.project = res.group(1)
+        # 解析custom配置
+        self.rules = custom["rules"]
+        # 生成防火墙策略
+        self.generate_route()
+        res = re.search(server_account_re, code, re.M|re.I)
+        self.server_account = res.group(1)
+        res = re.search(image_re, code, re.M|re.I)
+        self.image = res.group(1)
+        self.zone = custom["zone"]
+        self.code = custom["code"]
+        self.machine_type = custom["machine-type"]
+        self.disk_size = custom["disk-size"]
+        self.generate_vpc()
+        self.writetofile()
 
-routein = [
-    "auto-set-in",
-    "--description=\"auto set route firewall by script\"",
-    "--direction=INGRESS",
-    "--priority=1000",
-    "--network=default",
-    "--action=ALLOW",
-    "--rules={}",
-    "--source-ranges=0.0.0.0/0"
-]
+    def generate_route(self):
+        routebase = [
+            "gcloud",
+            "compute",
+            f"--project={self.project}",
+            "firewall-rules",
+            "create"
+        ]
 
-routeout = [
-    "auto-set-out",
-    "--description=\"auto set route firewall by script\"",
-    "--direction=EGRESS",
-    "--priority=1000",
-    "--network=default",
-    "--action=ALLOW",
-    "--rules={}",
-    "--destination-ranges=0.0.0.0/0"
-]
+        routein = [
+            "auto-set-in",
+            "--description=\"auto set route firewall by script\"",
+            "--direction=INGRESS",
+            "--priority=1000",
+            "--network=default",
+            "--action=ALLOW",
+            f"--rules={self.rules}",
+            "--source-ranges=0.0.0.0/0"
+        ]
 
-vpc = [
-    "gcloud",
-    "beta",
-    "compute",
-    "--project={}",
-    "instances",
-    "create",
-    "fenghshia-cloud",
-    "--zone={}",
-    "--machine-type=n1-highcpu-2",
-    "--subnet=default",
-    "--network-tier=PREMIUM",
-    "--metadata=\"ssh-keys={}\"",
-    "--maintenance-policy=MIGRATE",
-    "--service-account={}",
-    "--tags=auto-set-in,auto-set-out,http-server",
-    "--image=ubuntu-2004-focal-v20210119a",
-    "--image-project=ubuntu-os-cloud",
-    "--boot-disk-size={}GB",
-    "--boot-disk-type=pd-standard",
-    "--boot-disk-device-name=fenghshia-cloud",
-    "--no-shielded-secure-boot",
-    "--shielded-vtpm",
-    "--shielded-integrity-monitoring",
-    "--reservation-affinity=any"
-]
+        routeout = [
+            "auto-set-out",
+            "--description=\"auto set route firewall by script\"",
+            "--direction=EGRESS",
+            "--priority=1000",
+            "--network=default",
+            "--action=ALLOW",
+            f"--rules={self.rules}",
+            "--destination-ranges=0.0.0.0/0"
+        ]
 
+        base = deepcopy(routebase)
+        base.extend(routein)
+        self.routein = " ".join(base)
+        base = deepcopy(routebase)
+        base.extend(routeout)
+        self.routeout = " ".join(base)
+    
+    def generate_vpc(self):
+        vpc = [
+            "gcloud",
+            "beta",
+            "compute",
+            f"--project={self.project}",
+            "instances",
+            "create",
+            f"fenghshia-cloud{self.code}",
+            f"--zone={self.zone}",
+            f"--machine-type={self.machine_type}",
+            "--subnet=default",
+            "--network-tier=PREMIUM",
+            # "--metadata=\"ssh-keys={}\"",
+            "--maintenance-policy=MIGRATE",
+            f"--service-account={self.server_account}",
+            "--tags=auto-set-in,auto-set-out",
+            f"--image={self.image}",
+            "--image-project=ubuntu-os-cloud",
+            f"--boot-disk-size={self.disk_size}GB",
+            "--boot-disk-type=pd-standard",
+            f"--boot-disk-device-name=fenghshia-cloud{self.code}",
+            "--no-shielded-secure-boot",
+            "--shielded-vtpm",
+            "--shielded-integrity-monitoring",
+            "--reservation-affinity=any"
+        ]
+        
+        self.vpc = " ".join(vpc)
+    
+    def writetofile(self):
+        with open("vpcscript.sh", "w", encoding="utf-8") as f:
+            f.write(
+                f"{self.routein}\n{self.routeout}\n{self.vpc}"
+            )
 
-def setroutein(project, rules):
-    base = deepcopy(routebase)
-    base[2] = base[2].format(project)
-    routein[6] = routein[6].format(rules)
-    base.extend(routein)
-    return " ".join(base)
-
-
-def setrouteout(project, rules):
-    base = deepcopy(routebase)
-    base[2] = base[2].format(project)
-    routeout[6] = routeout[6].format(rules)
-    base.extend(routeout)
-    return " ".join(base)
-
-
-def setvpc(project, zone, sshkey, account, disk_size):
-    vpc[3] = vpc[3].format(project)
-    vpc[7] = vpc[7].format(zone)
-    vpc[11] = vpc[11].format(sshkey)
-    vpc[13] = vpc[13].format(account)
-    vpc[18] = vpc[18].format(disk_size)
-    return " ".join(vpc)
-
-def generatescript(project, rules, zone, sshkey, account, disk_size):
-    with open("vpcscript.sh", "w", encoding="utf-8") as f:
-        f.write(
-            setroutein(project, rules)+"\n"+setrouteout(project, rules)+"\n"+\
-                setvpc(project, zone, sshkey, account, disk_size)+"\n"
-        )
 
 if __name__ == "__main__":
-    project = "emerald-diagram-xxxxxx"
-    rules = "tcp:6000-7000,tcp:5050"
-    zone = "asia-east1-a"
-    sshkey = "fenghshia:ssh-rsa xxxxxx= fenghshia"
-    account = "xxxxxx-compute@developer.gserviceaccount.com"
-    disk_size = 10
+    # 需要改造: 1. 解析命令获取 项目名 账户名 镜像名 --完成
+    # 操作步骤: 1. 从gcp复制shell命令
+    #          2. 修改custom
+    #          3. 运行脚本
+    code = ""
+    custom = {
+        "rules": "tcp:80,tcp:6000-7000,tcp:8000-9000",
+        "zone": "asia-east1-a",
+        "code": datetime.now().strftime("%H%M%S"),
+        "machine-type": "n1-highcpu-2",
+        "disk-size": 50
+    }
 
-    generatescript(project, rules, zone, sshkey, account, disk_size)
+    vpccomplete(custom, code)
